998a892cd94b7210b38b810c182f9f5670552be9
victor <victorflu95123@gmail.com>
2025-11-18 18:52:22 -0300

    iniciando def

---
+++ main.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+
+def cadastrar_produto():
+    print("\n=== CADASTRAR PRODUTO ===")
+    nome = input("Nome: ")
+    categoria = input("Categoria: ")
+    preco = float(input("Preço: R$ "))
+    quantidade = int(input("Quantidade inicial: "))
+
+    data_pedido = datetime.today().date()
+    data_chegada = data_pedido + timedelta(days=7)  
+
+    print(f"\n✔ Produto cadastrado com sucesso!")
+    print(f"Data do pedido: {data_pedido} | Previsão de chegada: {data_chegada}\n")
+
+
+def excluir_produto():
+    print("\n=== EXCLUIR PRODUTO ===")
+    print("1 - Excluir por ID")
+    print("2 - Excluir por Nome")
+    opc = input("Escolha: ")
+
+
+def menu():
+    while True:
+        print("\n===== MENU ERP =====")
+        print("1 - Cadastrar produto")
+        print("2 - Excluir produto")
+        print("3 - Relatório de produtos")
+        print("4 - Sair")
+        opc = input("Escolha: ")
+
+        if opc == "1":
+            cadastrar_produto()
+        elif opc == "2":
+            excluir_produto()
+        elif opc == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
